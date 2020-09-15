dd3aaddf62fda756ce3a242a17d3dd98f0c22833
Llewellyn van der Merwe <llewellyn@vdm.io>
2020-09-15 19:02:49 +0200

    added the database update scripts for the new changes.

---
+++ admin/sql/updates/mysql/2.11.4.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `#__componentbuilder_joomla_module` CHANGE `default` `default` MEDIUMTEXT NOT NULL;
+
+ALTER TABLE `#__componentbuilder_custom_admin_view` CHANGE `default` `default` MEDIUMTEXT NOT NULL;
+
+ALTER TABLE `#__componentbuilder_site_view` CHANGE `default` `default` MEDIUMTEXT NOT NULL;
---
+++ admin/sql/updates/mysql/2.11.5.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `#__componentbuilder_joomla_component` ADD `remove_line_breaks` TINYINT(1) NOT NULL DEFAULT 0 AFTER `readme`;
