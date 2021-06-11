130235e252309742b0fd7580a6f5f5dad1238388
Llewellyn van der Merwe <llewellyn@vdm.io>
2021-06-11 02:48:03 +0200

    Stable release 2.12.10

--- script.php
+++ script.php
@@ -9252,7 +9252,7 @@ class com_componentbuilderInstallerScript
 			echo '<a target="_blank" href="http://www.joomlacomponentbuilder.com" title="Component Builder">
 				<img src="components/com_componentbuilder/assets/images/vdm-component.jpg"/>
 				</a>
-				<h3>Upgrade to Version 2.12.9 Was Successful! Let us know if anything is not working as expected.</h3>';
+				<h3>Upgrade to Version 2.12.10 Was Successful! Let us know if anything is not working as expected.</h3>';
 
 			// Set db if not set already.
 			if (!isset($db))
